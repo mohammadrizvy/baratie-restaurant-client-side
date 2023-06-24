3c7b7c3481d6939a3039fe8b091f9c299a935c10
mohammadrizvy <rizvyhq1@gmail.com>
2023-06-24 09:15:56 +0600

    Registration page done

--- src/Blogs/Blogs.jsx
+++ src/Blogs/Blogs.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div className='ms-8 me-8  font-extrabold text-7xl'>
-            Siuuuuuuuuuuuuu...................
+            Siuuuuuuuuuuuuu....................
         </div>
     );
 };
