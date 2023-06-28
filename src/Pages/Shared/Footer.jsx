29a39a36b7f735acc28fc5a3e54b7aa88fd5a769
mohammadrizvy <rizvyhq1@gmail.com>
2023-06-28 20:16:54 +0600

    Applied Lazyload on banner

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -2,40 +2,27 @@ import React from 'react';
 
 const Footer = () => {
     return (
-      <div>
-        <footer className="bg-secondary py-6 ms-8 me-8 rounded-md">
-          <div className="container mx-auto px-4">
-            <div className="flex flex-col items-center md:flex-row md:justify-between">
-              <div className="text-center md:text-left">
-                <p className="text-gray-300 hover:transform hover:scale-110 transition-transform duration-300">
-                  © 2023 Hotel Baraite. All rights reserved.
-                </p>
-              </div>
-              <div className="mt-4 md:mt-0">
-                <ul className="flex space-x-4">
-                  <li>
-                    <p className="text-gray-300 hover:text-white hover:transform hover:scale-110 transition-transform duration-300 ">
-                      About Us
-                    </p>
-                  </li>
-                  <li>
-                    <p className="text-gray-300 hover:text-white hover:transform hover:scale-110 transition-transform duration-300">
-                      Menu
-                    </p>
-                  </li>
-                  <li>
-                    <p className="text-gray-300 hover:text-white hover:transform hover:scale-110 transition-transform duration-300">
-                      Reservations
-                    </p>
-                  </li>
-                  <li>
-                    <p className="text-gray-300 hover:text-white hover:transform hover:scale-110 transition-transform duration-300">
-                      Contact
-                    </p>
-                  </li>
-                </ul>
-              </div>
-            </div>
+      <div className='me-8 ms-8 '>
+        <footer className="footer rounded-md p-10 bg-secondary text-white">
+          <div>
+            <span className="footer-title">Services</span>
+            <a className="link link-hover">Branding</a>
+            <a className="link link-hover">Design</a>
+            <a className="link link-hover">Marketing</a>
+            <a className="link link-hover">Advertisement</a>
+          </div>
+          <div>
+            <span className="footer-title">Company</span>
+            <a className="link link-hover">About us</a>
+            <a className="link link-hover">Contact</a>
+            <a className="link link-hover">Jobs</a>
+            <a className="link link-hover">Press kit</a>
+          </div>
+          <div>
+            <span className="footer-title">Legal</span>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
           </div>
         </footer>
       </div>
